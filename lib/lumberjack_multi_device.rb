b2e578a054baf478fecb3c0dc0238cff1774e0a6
Grant Rodgers <grantr@gmail.com>
2011-09-30 16:01:34 -0700

    Multi device

--- lib/lumberjack_multi_device.rb
+++ lib/lumberjack_multi_device.rb
@@ -1,5 +1,27 @@
 require "lumberjack_multi_device/version"
+require 'lumberjack'
 
-module LumberjackMultiDevice
-  # Your code goes here...
+module Lumberjack
+  class Device
+    class Multi < Device
+      attr_accessor :devices
+
+      def initialize(*args)
+        options = args.last.is_a?(Hash) ? args.pop : {}
+        @devices = args
+      end
+
+      def write(entry)
+        devices.each { |d| d.write(entry) }
+      end
+
+      def close
+        devices.each { |d| d.close }
+      end
+
+      def flush
+        devices.each { |d| d.flush }
+      end
+    end
+  end
 end
